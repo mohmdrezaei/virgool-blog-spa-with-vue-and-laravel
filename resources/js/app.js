8fb979841f5c9ef2598570f7abb58da993efb9c2
mohmdrezaei <mhwmdrz9@gmail.com>
2022-11-03 21:41:31 +0330

    refactor codes to composition api

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,12 @@ window.Vue = require('vue').default;
 
 import  vuetify  from "./plugin/vuetify"
 import router from "./router/router";
-
+import VueCompositionAPI from '@vue/composition-api';
 window.axios = require('axios');
 Vue.prototype.$http = window.axios;
+
+Vue.use(VueCompositionAPI)
+
 const app = new Vue({
     el: '#app',
     vuetify,
--- resources/js/view/auth/register.vue
+++ resources/js/view/auth/register.vue
@@ -4,7 +4,7 @@
             <auth-banner></auth-banner>
             <v-col cols="12" md="4" class="d-flex align-end">
                 <v-form class="w-100 mx-5"
-                        ref="form">
+                        ref="registerForm">
                     <span class="blue--text font-weight-bold">ایجاد حساب کاربری</span>
                     <v-text-field label="آدرس ایمیل"
                                   outlined
@@ -29,21 +29,23 @@
                                @click="register"
                         >
                             <template v-if="loading">
-                         <v-progress-circular
-                         color="white"
-                         :indeterminate="true"
-                         >
-                         </v-progress-circular>
+                                <v-progress-circular
+                                    color="white"
+                                    :indeterminate="true"
+                                >
+                                </v-progress-circular>
                             </template>
                             <template v-else>
-                                ایجاد حساب کاربری <v-icon class="mr-2">mdi-chevron-left</v-icon>
+                                ایجاد حساب کاربری
+                                <v-icon class="mr-2">mdi-chevron-left</v-icon>
                             </template>
                         </v-btn>
                     </div>
                     <div class="d-flex flex-column align-center justify-center mt-9 body-2">
                         <span class="mt-6">ورود با اکانت گوگل</span>
                         <span class="mt-6">ثبت نام در ویرگول به منزله موافقت با قوانین است</span>
-                        <router-link :to="{name:'login'}" class="my-6 mt-9">قبلا عضو شده اید؟ رفتن به صفحه ورود</router-link>
+                        <router-link :to="{name:'login'}" class="my-6 mt-9">قبلا عضو شده اید؟ رفتن به صفحه ورود
+                        </router-link>
                     </div>
                 </v-form>
             </v-col>
@@ -53,45 +55,56 @@
 
 <script>
 import AuthBanner from "@/components/auth/AuthBanner";
+import {ref} from "@vue/composition-api"
 
 export default {
     name: "register",
     components: {AuthBanner},
-    data(){
-        return{
-            form : {
-                email: null ,
-                password: null
-            },
-            errors: {
-                email : null,
-                password: null,
-            },
-            emailRules:[
-                value => !! value || 'ایمیل الزامی است' ,
-                value => /.+@+./.test(value) || 'یک ایمبل درست را وارد کنید',
-                value => (value ? value.length <= 255 : false) || 'طول ایمیل نباید بیشتر از 255 کاراکتر باشد'
-            ],
-            passwordRules:[
-                value => !! value || 'رمز عبور الزامی است' ,
-                value => (value ? value.length >= 8 : false) || 'طول رمز عبور باید بیشتر از 8 کاراکتر باشد'
-            ],
-            loading:false
+
+    setup() {
+        const registerForm = ref(null)
+        const form = ref({
+            email: null,
+            password: null
+        });
+        const errors = ref({
+            email: null,
+            password: null,
+        });
+
+        const emailRules = ref([
+            value => !!value || 'ایمیل الزامی است',
+            value => /.+@+./.test(value) || 'یک ایمبل درست را وارد کنید',
+            value => (value ? value.length <= 255 : false) || 'طول ایمیل نباید بیشتر از 255 کاراکتر باشد'
+        ]);
+        const passwordRules = ref([
+            value => !!value || 'رمز عبور الزامی است',
+            value => (value ? value.length >= 8 : false) || 'طول رمز عبور باید بیشتر از 8 کاراکتر باشد'
+        ]);
+        const loading = ref(false)
+   function register() {
+       if (registerForm.value.validate()) {
+           loading.value = true;
+           axios.post('/register', form.value)
+               .catch((error) => {
+                   errors.value.email = error.response.data.errors.email[0]
+                   errors.value.password = error.response.data.errors.password[0]
+               })
+               .finally(() => loading.value = false)
+       }
+   }
+        return {
+            form,
+            errors,
+            emailRules,
+            passwordRules,
+            loading,
+            registerForm,
+            register
         }
+
     },
-    methods:{
-        register(){
-            if (this.$refs.form.validate()){
-                this.loading=true;
-                axios.post('/register',this.form)
-                    .catch((error) => {
-                        this.errors.email = error.response.data.errors.email[0]
-                        this.errors.password = error.response.data.errors.password[0]
-                    })
-                    .finally( () => this.loading=false)
-            }
-        }
-    }
+
 }
 </script>
 
@@ -99,7 +112,6 @@ export default {
 .gra-bg {
     background-image: linear-gradient(to bottom right, #054592 0, #1897d4);
 }
-
 
 
 </style>
